20fb7ffc91f2c65aca237e742f475e828ffaa046
Talia Hagerty <talia@justreinvest.org.au>
2024-05-28 15:37:39 +1000

    added lga population

--- R/data.R
+++ R/data.R
@@ -19,3 +19,25 @@
 #' }
 #' @source <https://www.abs.gov.au/census/find-census-data/datapacks?release=2021&product=GCP&geography=POA&header=S>
 "population_postcode"
+
+#' Australian Bureau of Statistics 2021 Census General Community Profile for  Local Government Areas (LGA), NSW
+#'
+#' A subset of data from the 2021 Australian Census giving population by Aboriginality, five-year age cohort, and self-reported sex (binary). Unit of observation is the local government area (LGA).
+#' ABS LGAs are designed to approximate local government boundaries as closely as possible. They enable comparison of ABS data with other data collected using LGA as the geographic reference.
+#'
+#' NB: Small random adjustments have been made to all cell values in the csv files provided by the ABS, in order to protect the confidentiality of data.
+#' These adjustments may mean that totals (by age, sex, or geography) may differ slightly.
+#'
+#'
+#' @format ## `population_lga`
+#' A data frame with 5 columns:
+#' \describe{
+#'   \item{LGA_name}{Text names for NSW LGAs}
+#'   \item{LGA_CODE_2021}{ABS LGA codes for NSW}
+#'   \item{indicator}{Indicators giving the age cohort, sex, and Aboriginality, including totals}
+#'   \item{year}{Year}
+#'   \item{value}{Population value.}
+#'   ...
+#' }
+#' @source <https://www.abs.gov.au/census/find-census-data/datapacks?release=2021&product=GCP&geography=POA&header=S>
+"population_postcode"
---
+++ data-raw/population_lga.R
@@ -0,0 +1,40 @@
+## get LGA level population data
+
+library(readxl)
+library(tidyr)
+library(dplyr)
+
+
+# The population data is stored in a zip file on the ABS website:
+# https://www.abs.gov.au/census/find-census-data/datapacks?release=2021&product=GCP&geography=LGA&header=S
+
+
+## TODO: function or if statement to download data pack, extract file, use, remove
+  ## examples: https://github.com/hadley/babynames/tree/master/data-raw
+
+tmp <- read.csv("./data-raw/2021Census_G07_NSW_LGA.csv")
+
+# The ABS uses numeric LGA codes, which are in a different file
+
+# Set filepath for geographies xl
+xl_filepath <- "data-raw/2021Census_geog_desc_1st_2nd_3rd_release.xlsx"
+
+# Read the non-ABS geographies tab of the Excel file
+   # in this case, sheet 6
+# and then immediately filter for the codes we need
+codes <- read_excel(xl_filepath, sheet = 6) %>%
+  filter(ASGS_Structure == "LGA") %>%
+  select(Census_Code_2021, Census_Name_2021) %>%
+  rename(LGA_CODE_2021 = Census_Code_2021)
+
+
+population_lga <- tmp %>%
+  pivot_longer(!`LGA_CODE_2021`, names_to = "indicator", values_to = "value" ) %>%
+  mutate(year = "2021") %>%
+  left_join(codes, by = "LGA_CODE_2021") %>%
+  rename(LGA_name = `Census_Name_2021`) %>%
+  select(LGA_name, LGA_CODE_2021, indicator, year, value)
+
+
+usethis::use_data(population_lga, overwrite = TRUE)
+
