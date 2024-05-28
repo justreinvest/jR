ab577f5e1f4b3b9a08055ac3e76eef06245fa27e
Talia Hagerty <talia@justreinvest.org.au>
2024-05-28 15:52:08 +1000

    didn't save the correct files last time

--- R/lga population data documentation.R
+++ R/lga population data documentation.R
@@ -1,25 +1,4 @@
 
-#' Australian Bureau of Statistics 2021 Census General Community Profile for Postal Areas (POA), NSW
-#'
-#' A subset of data from the 2021 Australian Census giving population by Aboriginality, five-year age cohort, and self-reported sex (binary). Unit of observation is the postal area.
-#' Postal Areas (POAs) are designed to approximate postcode boundaries as closely as possible. They enable comparison of ABS data with other data collected using postcodes as the geographic reference.
-#'
-#' NB: Small random adjustments have been made to all cell values in the csv files provided by the ABS, in order to protect the confidentiality of data.
-#' These adjustments may mean that totals (by age, sex, or geography) may differ slightly.
-#'
-#'
-#' @format ## `population_postcode`
-#' A data frame with 4 columns:
-#' \describe{
-#'   \item{postal_area}{ABS Postal Areas for NSW}
-#'   \item{indicator}{Indicators giving the age cohort, sex, and Aboriginality, including totals}
-#'   \item{year}{Year}
-#'   \item{value}{Population value.}
-#'   ...
-#' }
-#' @source <https://www.abs.gov.au/census/find-census-data/datapacks?release=2021&product=GCP&geography=POA&header=S>
-"population_postcode"
-
 #' Australian Bureau of Statistics 2021 Census General Community Profile for  Local Government Areas (LGA), NSW
 #'
 #' A subset of data from the 2021 Australian Census giving population by Aboriginality, five-year age cohort, and self-reported sex (binary). Unit of observation is the local government area (LGA).
